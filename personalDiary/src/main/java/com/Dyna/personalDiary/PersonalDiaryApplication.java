36234f95c2d0e6424e4b1002d31d77b2eb89a69d
manodyna <77208955+manodyna@users.noreply.github.com>
2021-05-05 10:34:11 +0530

    a

--- personalDiary/src/main/java/com/Dyna/personalDiary/PersonalDiaryApplication.java
+++ personalDiary/src/main/java/com/Dyna/personalDiary/PersonalDiaryApplication.java
@@ -1,14 +1,30 @@
 package com.Dyna.personalDiary;
 
+import com.Dyna.personalDiary.Service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class PersonalDiaryApplication {
+public class PersonalDiaryApplication implements CommandLineRunner {
+
+	private EmailService emailService;
+
+	@Autowired
+	public void setEmailService(EmailService emailService) {
+		this.emailService = emailService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalDiaryApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+		emailService.prepareAndSendEmail();
 	}
 
 }
